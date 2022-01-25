45c00085abd459076db64d2fc763256fbf8a516a
ibidyouadu <angel.d.umana@gmail.com>
2022-01-24 19:56:18 -0500

    fix recoding values for q18 plots

--- R/plot_q18_fishing_high_profit.R
+++ R/plot_q18_fishing_high_profit.R
@@ -7,19 +7,19 @@ plot_q18_fishing_high_profit <- function(.data, ...){
     `18_fishing_high_profit`, 
     type = "stacked",
     key_name = "Frequency",
-    recoding = c("7 per week" = "Everyday",
-                 "7 times per week" = "Everyday",
-                 "6 times per week" = "Five or six times per week",
-                 "5-6 per week" = "Five or six times per week",
-                 "3-4 per week" = "Three or four times per week",
-                 "3-4 times per week" = "Three or four times per week",
-                 "1-2 per week" = "One or two times per week",
-                 "More than 1-2 times per week" = "More than few times per week"),
+    recoding = c("Everyday" = "7 per week",
+                 "Everyday" = "7 times per week",
+                 "5-6 times per week" = "6 times per week",
+                 "5-6 times per week" = "5-6 per week",
+                 "3-4 times per week" = "3-4 per week",
+                 "3-4 times per week" = "3-4 times per week",
+                 "1-2 times per week" = "1-2 per week",
+                 "More than few times per week" = "More than 1-2 times per week"),
     key_order = rev(c("Everyday",
-                      "Five or six times per week",
-                      "Three or four times per week",
+                      "5-6 times per week",
+                      "3-4 times per week",
                       "More than few times per week",
-                      "One or two times per week",
+                      "1-2 times per week",
                       "A few times per month",
                       "A few times" ,
                       "Once or never"))
@@ -27,7 +27,7 @@ plot_q18_fishing_high_profit <- function(.data, ...){
   
   p <- plot_bubble(
     .data_plot,
-    title = "Frequency that the main fisher go fishing \nduring the high profitable fishing season",
+    title = "Frequency that the main fisher goes fishing \nduring the high profitable fishing season",
     x_var = Frequency
   )
   
